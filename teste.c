4ced10018cebd700ee6ddb9d35ea03eb31db11ff
Henrique Pato Magalhães <89421003+Henrique-Pato@users.noreply.github.com>
2021-12-05 20:04:29 -0300

    Create teste.c

---
+++ teste.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+
+   int main(void) {
+   
+   //variáveis
+  float num1;float num2;
+
+   //leitura
+   printf("digite 2 números:");
+     scanf ("%f" "%f",&num1,&num2 );
+     
+     //condição
+      if(num1>num2)
+      printf ("O maior número é" "\t%f",num1);
+      else
+      printf ("O maior número é" "\t%f",num2);
+
+    
+     printf("\nfim do programa");
+     return 0;
+ }
